aa6fe6768885aecd87ed6d3c0eef2464d70cd7d1
bigdatacloudapi <deep@bigdatacloud.com>
2024-07-03 18:55:34 +0930

    change image card

--- components/content/PhotoCard.vue
+++ components/content/PhotoCard.vue
@@ -12,6 +12,8 @@ defineProps({
 </script>
 
 <template>
-    <img :src="href" :alt="caption">
-    <div>{{ caption }}</div>
+    <div class="border-b border-gray-200 border-dashed">
+        <img :src="href" :alt="caption" class="m-0 px-2 py-4" />
+        <div class="py-2 text-center text-sm italic">{{ caption }}</div>
+    </div>
 </template>
