54b0619e6af992f76b9bd70141c3afc1848ba023
bigdatacloudapi <deep@bigdatacloud.com>
2024-07-03 18:36:53 +0930

    added component

---
+++ components/content/PhotoCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    caption: {
+        type: String,
+        default: ''
+    },
+})
+</script>
+
+<template>
+    <img :src="href" :alt="caption">
+    <div>{{ caption }}</div>
+</template>
